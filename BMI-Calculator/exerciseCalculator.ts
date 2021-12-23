8fddaeaa7ac40b89d1c96bdb4cb9b9af8e15495a
tamanjic <tamanji.che@metropolia.fi>
2021-12-24 00:15:14 +0200

    Exercise 9.2: exerciseCalculator implemented for calculating the average daily hours spent exercising in a time period

---
+++ BMI-Calculator/exerciseCalculator.ts
@@ -0,0 +1,74 @@
+interface ExerciseStatistics {
+  periodLength: number;
+  trainingDays: number;
+  success: boolean;
+  rating: number;
+  ratingDescription: string;
+  target: number;
+  average: number;
+}
+
+interface Rating {
+  rating: number;
+  ratingDescription: string;
+}
+
+const calculateExercises = (
+  target: number,
+  exercises: Array<number>
+): ExerciseStatistics => {
+  const totalHours = exercises.reduce((acc, currentValue): number => {
+    if (currentValue < 0 || isNaN(currentValue))
+      throw new Error(
+        "Exercise hours must be numbers, nagative values not allowed!"
+      );
+    return acc + currentValue;
+  }, 0);
+
+  const periodLength = exercises.length;
+  const trainingDays: number = exercises.filter(
+    (hours: number) => hours !== 0
+  ).length;
+
+  if (periodLength === 0)
+    throw new Error("Operation not allowed, zero hours provided!");
+  const average = totalHours / periodLength;
+
+  // returns a value between 0 and 1; 1 => average hours equals original target (success, target met)
+  const successRatio = average / target;
+
+  const ratePerformance = (): Rating => {
+    switch (true) {
+      case successRatio >= 1:
+        return {
+          rating: 3,
+          ratingDescription:
+            successRatio === 1
+              ? "Excellent, keep up the good job!"
+              : "Fantastic! You've out-performed yourself, great job!",
+        };
+      case successRatio >= 0.5 && successRatio < 1:
+        return { rating: 2, ratingDescription: "Good, you could be better!" };
+      case successRatio < 0.5:
+        return { rating: 1, ratingDescription: "Bad, courage you can do it!" };
+    }
+  };
+
+  const { rating, ratingDescription } = ratePerformance();
+
+  return {
+    periodLength,
+    trainingDays,
+    success: successRatio >= 1,
+    rating,
+    ratingDescription,
+    target,
+    average,
+  };
+};
+
+try {
+  console.log(calculateExercises(2, [3, 0, 2, 5, 0, 3, 1]));
+} catch (e: unknown) {
+  if (e instanceof Error) console.log("ExerciseCalculationError: ", e.message);
+}
