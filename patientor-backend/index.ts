40eeef6ae8b61b09b301515b6d56e248f330a5fe
tamanjic <tamanji.che@metropolia.fi>
2021-12-26 23:26:08 +0200

    Exercise 9.9: Patientor backend, step 2 - backend now accepts ping request from Patientor frontend; cors installed and configured

--- patientor-backend/index.ts
+++ patientor-backend/index.ts
@@ -1,8 +1,13 @@
 import express from 'express';
+import cors from 'cors';
+
 const app = express();
+
+// eslint-disable-next-line @typescript-eslint/no-unsafe-call
+app.use(cors());
 app.use(express.json());
 
-const PORT = 3003;
+const PORT = 3001;
 
 app.get('/ping', (_req, res) => {
   console.log('you\'ve been pinged');
